513e941b88a3efd4a961df37015ef795fcf42111
JustinaJur <jurgeleviciute.justina@gmail.com>
2019-04-15 11:51:41 +0300

    update0

--- index.php
+++ index.php
@@ -1,5 +1,3 @@
 <?php include_once("seaharvest.html"); ?>
 
 
-///https://devcenter.heroku.com/articles/git#deploying-code
-//https://medium.com/@winnieliang/how-to-run-a-simple-html-css-javascript-application-on-heroku-4e664c541b0b
